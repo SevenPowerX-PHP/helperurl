fd301c70a10ac0ec3dc9cec98489a94fc999ae06
splaa-Acer <splaandrey@gmail.com>
2019-02-08 23:13:19 +0100

    First commit

---
+++ src/splx/helperurl/HelperURL.php
@@ -0,0 +1,30 @@
+<?php
+
+    namespace splx\helperurl;
+
+    class HelperURL
+    {
+        /**
+         * @return mixed
+         */
+
+         public function getCurPageURL()
+         {
+             $pageURL = 'http';
+             if(isset($_SERVER['HTTPS'])){
+                 if($_SERVER['HTTPS'] == 'on'){$pageURL .= "s";}
+             }
+             $pageURL .= '://';
+             if ($_SERVER['SERVER_PORT'] != '80') {
+                 $pageURL .= $_SERVER['SERVER_NAME'].':' 
+                 . $_SERVER['SERVER_PORT'] 
+                 . $_SERVER['SCRIPT_FILENAME'] 
+                 . $_SERVER['DOCUMENT_ROOT'];
+             }else{
+                 $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER['REQUEST_URI'];
+             }
+             return $pageURL;
+         }
+
+    }
+?>
